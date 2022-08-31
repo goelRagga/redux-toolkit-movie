927391e246ee44bbebfafce75345f8fb5aa51eea
unknown <raghavgoel847@gmail.com>
2022-08-31 12:16:32 +0530

    Redux Toolkit Setuo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,8 +3,15 @@ import {MovieListing} from "../index"
 import movieApi from "../../common/services/apis/movieApi"
 import "./Home.scss"
 import {APIKEY} from "../../common/services/apis/MovieApiKey"
+import {useDispatch,useSelector} from "react-redux"
+import { addMovies } from '../../features/movies/movieSlice'
 const Home = () => {
 
+
+   // Dispath Object
+   const dispatch = useDispatch();
+
+  // Fetching Data on Mounting
   React.useEffect(()=>{
     const movieText= "Harry"
     const fetchMovies = async() =>{
@@ -12,11 +19,13 @@ const Home = () => {
       .catch((err)=>{
         console.log("error",err);
       })
-
       console.log("Response",response)
+      dispatch(addMovies(response.data))
     };
     fetchMovies() 
   },[])
+
+ 
   
   return (
     <>
