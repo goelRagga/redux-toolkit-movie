bf812d4cb231d5ffefb79597ea6b907d7f848499
unknown <raghavgoel847@gmail.com>
2022-08-31 11:17:59 +0530

    Data Fetched using Axios

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,30 @@
-import React from 'react'
-
+import * as React from 'react'
+import {MovieListing} from "../index"
+import movieApi from "../../common/services/apis/movieApi"
+import "./Home.scss"
+import {APIKEY} from "../../common/services/apis/MovieApiKey"
 const Home = () => {
+
+  React.useEffect(()=>{
+    const movieText= "Harry"
+    const fetchMovies = async() =>{
+      const response = await movieApi.get(`?apiKey=${APIKEY}&s=${movieText}&type=movie`)
+      .catch((err)=>{
+        console.log("error",err);
+      })
+
+      console.log("Response",response)
+    };
+    fetchMovies() 
+  },[])
+  
   return (
-    <div>Home</div>
+    <>
+    <div className='banner-img'>
+      <MovieListing />
+    </div>
+    </>
+    
   )
 }
 
